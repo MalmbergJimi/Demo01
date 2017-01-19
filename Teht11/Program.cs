e5d0e38d543531c882295fde653168694b33ff1a
K8718 Jimi M. <K8718@student.jamk.fi>
2017-01-19 15:29:06 +0200

    Viimeiset rivit tuplana

---
+++ Teht11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rivit, i, h;
+            string tahti = "*";
+
+            Console.WriteLine("Montako tähtiriviä haluat? ");
+            string line = Console.ReadLine();
+            rivit = int.Parse(line);
+
+            
+
+            for (i=0; i < rivit; i++)
+            {
+                Console.WriteLine(tahti);
+
+                for (h=0; h<i+1; h++)
+                {
+                 Console.Write(tahti);
+                }
+            }
+        }
+    }
+}
